f7d2c0e9a3f06160977d46263c5ea2ad87dacce8
Nikhil Patel <nikhil.p@hotmail.co.uk>
2017-10-08 13:27:29 +0100

    Initial commit

---
+++ MainMenuPGU.py
@@ -0,0 +1,68 @@
+
+import pygame
+from pygame.locals import *
+from pgu import gui
+pygame.init()
+
+
+BLACK   = (  0,  0,  0)
+WHITE   = (255,255,255)
+BLUE    = (  0,  0,255)
+GREEN   = (  0, 255, 0)
+RED     = (255,   0, 0)
+PURPLE  = (255,  0,255)
+def main():
+    app = gui.Desktop(width = 800,height = 600)
+    app.connect(gui.QUIT,app.quit,None)
+
+        
+    def createButton2(imgName,text,adj):
+        firstpart, secondpart = text[:len(text)//2 +adj], text[len(text)//2 +adj:]
+        main = gui.Table()
+        main.tr()
+        main.td(gui.Image(imgName))
+        main.tr()
+        main.td(gui.Label(firstpart))
+        main.tr()
+        main.td(gui.Label(secondpart))
+        return gui.Button(main,height = 130,width = 200)
+
+
+    font = pygame.font.SysFont("comicsansms", 60)
+    text = font.render("Main Menu",True, (0, 128, 0))
+
+    menuTable = gui.Table(width = 800,height = 600)
+
+    RofMetalImg = pygame.image.load("ResistivityOfAMetal.jpg")
+    RofMetalImg = pygame.transform.scale(RofMetalImg,(256,200))
+    IRImg = pygame.image.load("InternalResistance.jpg")
+    IRImg = pygame.transform.scale(IRImg,(350,200))
+
+    def cbFunc():
+        app.quit()
+        import TestExperimentPGU as exp1
+        exp1.main()
+    a = createButton2("Ball1.png","Determination of the Resistivity of a Metal",0)
+    a.connect(gui.CLICK,cbFunc)
+
+
+    menuTable.tr()
+    menuTable.td(gui.Label(""))
+    menuTable.td(gui.Image(text))
+    menuTable.td(gui.Button("Help",height = 50,width = 100))
+    menuTable.tr()
+    menuTable.td(a)
+    menuTable.td(createButton2("Ball1.png","Determination of the Internal Resistance of a Cell",4))
+    menuTable.td(createButton2("Ball1.png","Estimation of Absolute Zero by Use of the Gas Laws",6))
+    menuTable.tr()
+    menuTable.td(createButton2("Ball1.png","Investigation of Newton’s Second Law",-1))
+    menuTable.td(createButton2("Ball1.png","Investigation of Radioactive Decay",0))
+    menuTable.td(createButton2("Ball1.png","Measurement of g by free-fall",0))
+    menuTable.tr()
+    menuTable.td(createButton2("Ball1.png","Measurement of the Specific Heat Capacity for a Solid",1),col = 1)
+
+
+
+    app.run(menuTable)
+
+main()
---
+++ TestExperiment.py
@@ -0,0 +1,84 @@
+import pygame
+
+BLACK   = (  0,  0,  0)
+WHITE   = (255,255,255)
+BLUE    = (  0,  0,255)
+GREEN   = (  0, 255, 0)
+RED     = (255,   0, 0)
+PURPLE  = (255,  0,255)
+
+sWidth,sHeight = 800,600
+
+pygame.init()
+
+screen = pygame.display.set_mode([sWidth,sHeight])
+
+clock = pygame.time.Clock()
+done = False
+
+
+class Experiment(object):
+    def __init__(self,equipment):
+        self.drawEquipment = pygame.sprite.Group()
+        self.particles = pygame.sprite.Group()
+        for i in equipment:
+            self.drawEquipment.add(i)
+
+##    def checkCollision(self):
+    
+            
+            
+
+class Equipment(pygame.sprite.Sprite):
+    def __init__(self,image,x,y):
+        super().__init__()
+        self.image = image
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("ball1.png")
+        self.image = pygame.transform.scale(self.image,(20,20))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = sWidth/2
+        self.rect.y = sHeight/2
+        self.xSpeed = 5
+        self.ySpeed = 5
+
+
+beakerImg = pygame.image.load("beaker.png")
+newDim = beakerImg.get_rect().width*3,beakerImg.get_rect().height*3
+beakerImg = pygame.transform.scale(beakerImg,(newDim))
+beaker = Equipment(beakerImg,(sWidth-newDim[0])/2 ,(sHeight-newDim[1])/2)
+exp = Experiment([beaker])
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                exp.particles.add(Ball())
+
+    for i in exp.particles:
+        i.rect.x += i.xSpeed
+        i.rect.y += i.ySpeed
+    
+    screen.fill(WHITE)
+    exp.drawEquipment.draw(screen)
+    exp.particles.draw(screen)
+
+    
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+    
+
+            
