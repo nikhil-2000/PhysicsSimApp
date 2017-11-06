d344bc3fae7bb8250bd8b2e4bc75b18d2c7f86b3
Nikhil Patel <nikhil.p@hotmail.co.uk>
2017-11-06 18:04:14 +0000

    Added comments to my validation function and its tests

--- Validation/validation.py
+++ Validation/validation.py
@@ -1,50 +1,47 @@
-#Validation Tests
-def checkInt(num):
+#Validation
+def checkInt(num):  #Confirms if the string passed in is made up of only integers
     if str.isdigit(num):
         return True
     return False
           
 
-def validateInputs (minIV,maxIV,interval):
-    minRange = 0
-    maxRange = 100
-    inputs = [minIV,maxIV,interval]
+def validateInputs (minIV,maxIV,interval,maxRange,minRange):
+    inputs = [minIV,maxIV,interval]  #Makes it easy to check all the inputs using for loops
     emptyCheck = []
+    #Checks that none of the inputs are empty
     for i in range(0,3):
         if inputs[i] == "":
             emptyCheck.append(True)
 
     
     if any(emptyCheck):
-        print("Type something in the boxes")
+        print("Type something in the boxes")#Error returned
         return False
     
     intCheck = []
-    for i in range(0,3):
+    for i in range(0,3):  #Uses function checkInt()
         if checkInt(inputs[i]):
             intCheck.append(True)
         else:
             intCheck.append(False)
 
-    print(intCheck)
-    if all(intCheck):
+    if all(intCheck):  #If all are only numbers, convert them to intergers
             maxIV = int(maxIV)
             minIV = int(minIV)
             interval = int(interval)
     else:
             print("Enter integers only")
             return False
-        
-        
-    if minIV >= maxIV:
+
+    if minIV >= maxIV:  #Ensures the minimum isn't larger than the maximum
             print("Your minimum value should be smaller than the maximum value")
             return False
 
-    if minIV < minRange or maxIV > maxRange:
+    if minIV < minRange or maxIV > maxRange:  #Checks that both numbers are within the range
             print("Both the minimum and maximum should be between 0-100")
             return False
         
-    noOfIntervals = ((maxIV - minIV) // interval)  + 1
+    noOfIntervals = ((maxIV - minIV) // interval)  + 1 #Calculates the number of intervals
     if noOfIntervals < 5:
             print("Make your range larger or decrease the interval")
             return False
@@ -54,11 +51,12 @@ def validateInputs (minIV,maxIV,interval):
             return False
 
     intervalMatch = (maxIV - minIV) % interval == 0
+    #This will be true if the interval lines up with the max value
     
     if not(intervalMatch):
-        errorMsg = "Change the interval or maximum value as the values don't line up"
+        print("Change the interval or maximum value as the values don't line up")
         return False
     
-    return True
+    return True  #If all the previous checks have passed, return true
 
 
--- tests/validationTests.py
+++ tests/validationTests.py
@@ -1,43 +1,62 @@
-import validationChecks as v
-import unittest
+import os
+import sys
+sys.path.append(os.path.abspath('..'))
+#Allows me to access the file above and import scripts/modules from the root
+
+import Validation.validation as v#Imports the file where the validation algorithm is held
+import unittest #The python module for writing unit tests
+import time#The python module for time
 
 class TestValidationFunction(unittest.TestCase):
-    def test_normalInputs(self):
-        validInputs = v.validateInputs("10","100","10")
+    def setUp(self):  #This method is ran before each test
+        #These two variables are constant for each test
+        self.minRange = 0
+        self.maxRange = 100
+        #Gives a clearer output of error messages for each test
+        time.sleep(1)
+
+    def test_normalInputs(self):  #Tests that the normal values are accepted by the algorithm
+        validInputs = v.validateInputs("10","100","10",self.maxRange,self.minRange)
         self.assertEqual(validInputs,True)
 
-    def test_floatInputs(self):
-        validInputs = v.validateInputs("3.2","4.2","0.2")
+    def test_emptyCheck(self):  #Tests that the inputs aren't empty
+        validInputs = v.validateInputs("","","",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)
 
-    def test_negativeInputs(self):
-        validInputs = v.validateInputs("-12","0","2")
+    def test_floatInputs(self):  #Tests that floats are rejected
+        validInputs = v.validateInputs("3.2","4.2","0.2",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)
 
-    def test_moreThan10Intervals(self):
-        validInputs = v.validateInputs("0","11","1")
+    def test_negativeInputs(self):  #Tests that negative numbers are rejected
+        validInputs = v.validateInputs("-12","0","2",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)
 
-    def test_lessThan5Intervals(self):
-        validInputs = v.validateInputs("0","30","10")
+    def test_moreThan10Intervals(self):  #Tests that there aren't too many intervals
+        validInputs = v.validateInputs("0","11","1",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)
 
-    def test_lowerThanRange(self):
-        validInputs = v.validateInputs("-10","60","10")
+    def test_lessThan5Intervals(self):  #Tests that there aren't too little intervals
+        validInputs = v.validateInputs("0","30","10",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)
 
-    def test_higherThanRange(self):
-        validInputs = v.validateInputs("90","160","10")
+    def test_lowerThanRange(self):  #Tests that IV can't be inputted below the minimum range
+        validInputs = v.validateInputs("-10","60","10",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)
 
-    def test_intervalMatch(self):
-        validInputs = v.validateInputs("75","90","2")
+    def test_higherThanRange(self):  #Tests that the IV can't be inputted above the maximum range
+        validInputs = v.validateInputs("90","160","10",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)
 
-    def test_minMoreThanMax(self):
-        validInputs = v.validateInputs("60","50","0")
+    def test_intervalMatch(self):  #Tests that the interval and the max range match
+        # For example, if I kept adding 2 to 75, you wouldn't arrive at 90
+        # This tests ensures that the numbers align correctly
+        validInputs = v.validateInputs("75","90","2",self.maxRange,self.minRange)
+        self.assertEqual(validInputs,False)
+
+    def test_minMoreThanMax(self):  #Tests that the maximum value for the IV is below the minimum value
+        validInputs = v.validateInputs("60","50","0",self.maxRange,self.minRange)
         self.assertEqual(validInputs,False)        
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity = 2)  #Presents the test in a more readable way
